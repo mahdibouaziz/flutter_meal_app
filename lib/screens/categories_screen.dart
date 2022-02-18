649db5e30b76245e5e5c8375a1565c6d3a2998d5
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-02-18 17:20:13 +0100

    fixed the project structure

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meal_app/category_item.dart';
+import 'package:flutter_meal_app/widgets/category_item.dart';
 import 'package:flutter_meal_app/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class MealItem extends StatelessWidget {
+  const MealItem({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
